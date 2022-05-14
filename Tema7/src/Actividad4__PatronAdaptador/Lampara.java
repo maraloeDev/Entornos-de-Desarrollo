abae60160ac15dcac17a39e6eae7b6e5d00a8343
Eduardo <Eduardo@host.docker.internal>
2022-05-14 19:32:24 +0200

    Ejercicio 3

---
+++ Tema7/src/Actividad4__PatronAdaptador/Lampara.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividad4__PatronAdaptador;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Lampara {
+    
+   private boolean encendido;
+    
+    public  void estaencendido(){
+        
+        encendido = true;
+        
+    }
+    
+    public void estaApagado(){
+        
+        encendido = false;
+
+    }
+    
+}
---
+++ Tema7/src/Actividad4__PatronAdaptador/LamparaInglesa.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividad4__PatronAdaptador;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class LamparaInglesa {
+
+    public boolean isOn;
+
+    public boolean isOn() {
+
+        return isOn;
+    }
+
+    public void on() {
+        isOn = true;
+
+    }
+
+    public void isOff() {
+        isOn = false;
+    }
+}
