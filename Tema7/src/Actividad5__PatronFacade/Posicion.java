d0236814d14b61fad6df0c52fe9292328eeefe5d
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-05-25 09:39:39 +0200

    Actividad5

--- Tema7/src/Actividad5__PatronFacade/Posicion.java
+++ Tema7/src/Actividad5__PatronFacade/Posicion.java
@@ -14,4 +14,43 @@ public class Posicion {
     private String Longitud;
     private String Altura;
     private String Fecha;
+
+    public String getLatitud() {
+        return Latitud;
+    }
+
+    public void setLatitud(String Latitud) {
+        this.Latitud = Latitud;
+    }
+
+    public String getLongitud() {
+        return Longitud;
+    }
+
+    public void setLongitud(String Longitud) {
+        this.Longitud = Longitud;
+    }
+
+    public String getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(String Altura) {
+        this.Altura = Altura;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Posicion{" + "Latitud=" + Latitud + ", Longitud=" + Longitud + ", Altura=" + Altura + ", Fecha=" + Fecha + '}';
+    }
+    
+    
 }
---
+++ Tema7/src/Actividad5__PatronFacade/WiFi.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividad5__PatronFacade;
+
+/**
+ *
+ * @author maraloed
+ */
+public class WiFi {
+    
+    public String wifi;
+    
+    public void Encender(){
+        System.out.println("WIFI, encendido");
+    }
+    
+    public void Conectar (String SSID){
+        
+        wifi=SSID;
+        System.out.println("Conectar a WIFI " + SSID);
+    }
+    public void Desconectar(){
+        System.out.println("Desconectar WIFI " + wifi);
+    }
+    public void Apagar(){
+        System.out.println("Wifi apagada");
+    }
+
+    public String getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(String wifi) {
+        this.wifi = wifi;
+    }
+    
+    
+}
